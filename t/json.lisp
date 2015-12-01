1923aee9b47d13d0811b1a94c7ce6177dfbc23bc
brharp <brharp@uoguelph.ca>
2015-11-30 21:28:50 -0500

    Added run-test function.

--- t/json.lisp
+++ t/json.lisp
@@ -1,8 +1,29 @@
 (load "../json")
+(in-package :cl-user)
 
-(with-input-from-string
- (*standard-input* "123")
- (assert (equal (json-number) 123)))
+(defpackage #:json-test
+  (:use #:json #:common-lisp)
+  (:import-from #:json #:json-number #:json-array #:json-object)
+  )
+
+(in-package #:json-test)
+
+(defun run-test (test)
+  (let ((exp (copy-tree (first test)))
+	(input (getf (rest test) ':input))
+	(expec (getf (rest test) '=> ':anything))
+	(result nil))
+    (if input
+      (with-input-from-string (*standard-input* input)
+	(setf result (eval exp)))
+      (setf result (eval exp)))
+    (unless (or (equal expec ':anything)
+		(equal expec result))
+      (format *terminal-io*
+	      "~%**** FAILED: ~S~%  expected: ~S~%  got: ~S~%"
+	      exp expec result))))
+
+(run-test '((json-number) => 123 :input "123"))
 
 (with-input-from-string
  (*standard-input* "-123.0")
@@ -34,3 +55,4 @@
    (handler-case 
        (and (json-array) nil)
      (condition () t))))
+
